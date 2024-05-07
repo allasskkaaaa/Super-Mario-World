7206820e73941025e9916176a921314fa2d85618
allasskkaaaa <lexiecuvin@gmail.com>
2024-05-07 02:16:23 -0400

    Lab 4 done

--- Assets/Scripts/Mechanics/Projectile.cs
+++ Assets/Scripts/Mechanics/Projectile.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Projectiles : MonoBehaviour
 {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player") && !collision.gameObject.CompareTag("PowerUp") && !collision.gameObject.CompareTag("Collectible"))
+            Destroy(gameObject);
+    }
     public float lifetime;
 
     //Speed value is set by shoot script when the player fires
